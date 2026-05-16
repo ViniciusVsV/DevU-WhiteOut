3c9cb2b34829060e4974413beab985f470c65c8d
ViniciusVsV <viniciusvsv193@gmail.com>
2026-05-16 06:58:33 -0300

    Termianndo de configurar as ultimas fases + cutscene final

--- Assets/Levels/Level Final/Final Cutscene.cs
+++ Assets/Levels/Level Final/Final Cutscene.cs
@@ -1,16 +1,58 @@
+using System.Collections;
+using Entities.Player;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
-public class FinalCutscene : MonoBehaviour
+namespace Levels.LevelFinal
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class FinalCutscene : MonoBehaviour
     {
-        
-    }
+        private BoxCollider2D col;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private InputHandler inputHandler;
+        [SerializeField] private CinemachineCamera cinemachineCamera;
+        [SerializeField] private CinemachineBrain mainCamera;
+        [SerializeField] private PlayableDirector timeline;
+        [SerializeField] private AudioSource musicSource;
+
+        private void Awake()
+        {
+            col = GetComponent<BoxCollider2D>();
+
+            musicSource.volume = 0;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                col.enabled = false;
+
+                //Desativa os inputs do jogador
+                inputHandler.DisableInputs();
+
+                StartCoroutine(Routine());
+            }
+        }
+
+        private IEnumerator Routine()
+        {
+            //Troca a câmera
+            cinemachineCamera.Priority = 100;
+
+            //Espera a transição da câmera finalizar
+            yield return new WaitForSeconds(mainCamera.DefaultBlend.Time);
+
+            //Dá play na timeline
+            timeline.Play();
+        }
+
+        public void ReturnToMenu()
+        {
+            SceneManager.LoadScene("Level 0");
+        }
     }
 }
--- Assets/Menu System/Scripts/Sections/Pause Section.cs
+++ Assets/Menu System/Scripts/Sections/Pause Section.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace MenuSystem.Sections
 {
     public class PauseSection : MonoBehaviour
     {
         [SerializeField] private GameObject sectionObject;
+        [SerializeField] private GameObject startingObject;
 
         public void Activate()
         {
             sectionObject.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(startingObject);
         }
 
         public void Deactivate()
